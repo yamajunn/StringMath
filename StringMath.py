0bdfca26798d70be0bb0b6c9b2588eb923544156
yamajunn <amiton1115@gmail.com>
2024-07-19 15:15:55 +0900

    refactor: Improve efficiency of string addition and subtraction functions

--- StringMath.py
+++ StringMath.py
@@ -25,28 +25,35 @@ def plus(a, b):
 
     max_len = len(a)
     answer = []
-    carry = "0"
+    add = "0"
 
     for i in range(max_len - 1, -1, -1):
-        cal = MINUS_DICT[f"{a[i]}-{b[i]}"]
-        if cal == "-1":
-            carry = "1"
-            a[i-1] = str(int(a[i-1]) - 1)  # 前の桁から1を引く
-            cal = MINUS_DICT[f"1{a[i]}-{b[i]}"]
+        cal = PLUS_DICT[f"{a[i]}+{b[i]}"]
+        last_cal = cal[-1]
+        last_cal = PLUS_DICT[f"{last_cal}+{add}"]
+        if len(last_cal) > 1:
+            add = PLUS_DICT[f"{cal[0]}+{last_cal[0]}"][0]
+        elif len(cal) > 1:
+            add = cal[0]
         else:
-            carry = "0"
-        answer.insert(0, cal[-1])
+            add = "0"
+        answer.insert(0, last_cal[-1])
 
-    answer = ''.join(answer)
-    answer = answer.lstrip("0")
+    if add != "0":
+        max_int += 1
+        answer.insert(0, add)
 
-    if not answer:
-        return "0"
-
-    if "." in answer:
-        answer = answer.rstrip("0").rstrip(".")
-
-    return answer
+    answer.insert(max_int, ".")
+    
+    while len(answer) > 1 and answer[0] == '0' and answer[1] != '.':
+        answer.pop(0)
+    if '.' in answer:
+        while answer[-1] == '0':
+            answer.pop()
+        if answer[-1] == '.':
+            answer.pop()
+    
+    return "".join(answer)
 
 def plus_test(a, b):
     return a+b
@@ -102,22 +109,22 @@ def minus(a, b):
 
 # a = random.random()
 # b = random.random()
-a = 3.1
-b = 1.2
-
-# print("String Math")
-# print(f"{a} + {b} = {plus(a, b)}")
-
-# print("NumPy Math")
-# print(f"{a} + {b} = {np.add(a, b)}")
-
-# print("Python Math")
-# print(f"{a} + {b} = {a + b}")
+a = 0.1
+b = 0.2
 
 print("String Math")
-print(f"{a} - {b} = {minus(a, b)}")
+print(f"{a} + {b} = {plus(a, b)}")
+
+print("NumPy Math")
+print(f"{a} + {b} = {np.add(a, b)}")
 
 print("Python Math")
-print(f"{a} - {b} = {a - b}")
+print(f"{a} + {b} = {a + b}")
+
+# print("String Math")
+# print(f"{a} - {b} = {minus(a, b)}")
+
+# print("Python Math")
+# print(f"{a} - {b} = {a - b}")
 
 # print(0.1+0.2)
